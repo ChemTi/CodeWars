da85b99b821457f56e45dde0d8cdb2035154b94f
ChemTi <79016313+ChemTi@users.noreply.github.com>
2022-02-12 14:17:08 +0300

    Update Reversed Strings.js

--- Reversed Strings.js
+++ Reversed Strings.js
@@ -1,5 +1,26 @@
 function solution(str) {
-  return str.split('').reverse().join('');
+  return str.split("").reverse().join("");
 }
 
-console.log(solution('-1asdf3+'));
+console.log(solution("-1asdf3+"));
+
+//const solution = str => [...str].reverse().join('')
+
+/* function solution(str){
+  var o = '';
+  for (var i = str.length - 1; i >= 0; i--)
+    o += str[i];
+  return o;
+} */
+
+/* function solution(str){
+  return str.length > 0 ? solution(str.substring(1)) + str.charAt(0) : '';
+} */
+
+/* function solution(str){
+  let n = ''
+  for(let i of str){
+    n = i+n
+  }
+  return n
+} */
