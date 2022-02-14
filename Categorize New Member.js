b196a8988fdde204cd25f9da6ec6faf32290a7dd
Timur_Chemazokov <79016313+ChemTi@users.noreply.github.com>
2022-02-14 21:34:23 +0300

    Update Categorize New Member.js

--- Categorize New Member.js
+++ Categorize New Member.js
@@ -12,10 +12,55 @@ input =  [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
 output = ["Open", "Open", "Senior", "Open", "Open", "Senior"] */
 
 function openOrSenior(data){
-    function determineMembership(member){
-        return (member[0] >= 55 && member[1] > 7) ? 'Senior' : 'Open';
-      }
-      return data.map(determineMembership);
+	function determineMembership(member){
+		return (member[0] >= 55 && member[1] > 7) ? 'Senior' : 'Open';
+	  }
+	  return data.map(determineMembership);
   }
 
   console.log([[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]);
+
+/* function openOrSenior(data){
+  return data.map(([age, handicap]) => age >= 55 && handicap > 7 ? 'Senior' : 'Open');
+} */
+
+
+/* function openOrSenior(data){
+  return data.map(([age, handicap]) => (age > 54 && handicap > 7) ? 'Senior' : 'Open');
+} */
+
+
+/* function openOrSenior(data){
+  // Senior = 55+ and 7+
+  var result = [];
+
+  for(var i = 0; i < data.length; i++){
+    result[i] = (data[i][0] >= 55 && data[i][1] > 7) ? 'Senior' : 'Open';
+  }
+
+  return result;
+} */
+
+/* function openOrSenior(data){
+  var result = [];
+  data.forEach(function(member) {
+    if(member[0] >= 55 && member[1] > 7) {
+      result.push('Senior');
+    } else {
+      result.push('Open');
+    }
+  })
+  return result;
+} */
+
+/* const openOrSenior = data => {
+  let getOpenOrSenior = [];
+  for(i=0; i<data.length; i++){
+    if(data[i][0]>=55 && data[i][1]>7){
+      getOpenOrSenior.push("Senior");
+    }else{
+      getOpenOrSenior.push("Open");
+    }
+  }
+  return getOpenOrSenior;
+} */
