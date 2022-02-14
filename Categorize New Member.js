e8fea31223736823dfc94099d96f3e3385cc1cdd
Timur_Chemazokov <79016313+ChemTi@users.noreply.github.com>
2022-02-14 21:49:08 +0300

    Update Categorize New Member.js

--- Categorize New Member.js
+++ Categorize New Member.js
@@ -18,7 +18,7 @@ function openOrSenior(data){
 	  return data.map(determineMembership);
   }
 
-  console.log([[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]);
+  console.log([[45, 12],[55,21],[19, -2],[104, 20]]);
 
 /* function openOrSenior(data){
   return data.map(([age, handicap]) => age >= 55 && handicap > 7 ? 'Senior' : 'Open');
