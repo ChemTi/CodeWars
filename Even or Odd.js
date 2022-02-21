3b302b1851d18570ed0de7fa9c92283576f490a6
ChemTi <79016313+ChemTi@users.noreply.github.com>
2022-02-21 20:57:34 +0300

    Update Even or Odd.js

--- Even or Odd.js
+++ Even or Odd.js
@@ -9,3 +9,26 @@ function even_or_odd(number) {
 }
 
 console.log(even_or_odd(0));
+
+/* function even_or_odd(number) {
+  if (Number.isInteger(number) == true) {
+    if (number % 2 == 0) {
+      return "Even";
+    } else {
+      return "Odd";
+    }
+  }
+} */
+
+
+/* function even_or_odd(number) {
+  return number % 2 ? "Odd" : "Even"
+} */
+
+/* const even_or_odd = n => (n % 2) ? 'Odd' : 'Even'; */
+
+/* function even_or_odd(number) {
+  return ( (number % 2) == 0 ) ? "Even" : "Odd";
+} */
+
+/* var even_or_odd = n => n & 1 ? 'Odd' : 'Even' */
