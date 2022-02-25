2c97b9571e7058abadc061317d8bdbc7b4bf172e
ChemTi <79016313+ChemTi@users.noreply.github.com>
2022-02-25 20:19:44 +0300

    additionals

--- A Needle in the Haystack.js
+++ A Needle in the Haystack.js
@@ -11,7 +11,62 @@ findNeedle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk'])
 should return "found the needle at position 5" (in COBOL "found the needle at position 6")*/
 
 function findNeedle(haystack) {
-    return 'found the needle at position ' + haystack.indexOf('needle')
+  return "found the needle at position " + haystack.indexOf("needle");
 }
 
-console.log(findNeedle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'tim' , 'needle', 'randomJunk']))
+console.log(
+  findNeedle([
+    "hay",
+    "junk",
+    "hay",
+    "hay",
+    "moreJunk",
+    "tim",
+    "needle",
+    "randomJunk",
+  ])
+);
+
+/* const findNeedle = haystack => `found the needle at position ${haystack.indexOf('needle')}`; */
+
+/* function findNeedle(haystack) {
+  var i = 0;
+  while (i < haystack.length) {
+    if (haystack[i] == 'needle') {
+      return 'found the needle at position ' + i;
+    }
+    i++;  
+  }
+} */
+
+/* function findNeedle(array) {
+for (var i=0; i<array.length; i++){
+  if (array[i] === 'needle')
+    return 'found the needle at position ' + i;
+}
+  // your code here
+} */
+
+/* function findNeedle(haystack) {
+  let item = null;
+    const array1 = haystack;
+    array1.forEach((currentValue, index) => {    
+      if(currentValue ==='needle') {
+        item = 'found the needle at position ' + String(index);
+      }
+    });
+    return item;
+  }; */
+
+/* function findNeedle(q,q2='needle') {
+  let a=q.findIndex(e=>e==q2);
+ return(a>-1)?`found the ${q2} at position ${a}`:``;
+} */
+
+/* function findNeedle(x) {
+  var y = 0;
+  while (x[y] !== "needle") {
+    y++;
+  }
+      return ("found the needle at position " + y);
+} */
