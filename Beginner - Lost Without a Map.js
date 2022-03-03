11b756a3975e4b305583a3a95d48a564c12438c4
ChemTi <79016313+ChemTi@users.noreply.github.com>
2022-03-03 20:27:12 +0300

    Update Beginner - Lost Without a Map.js

--- Beginner - Lost Without a Map.js
+++ Beginner - Lost Without a Map.js
@@ -7,10 +7,46 @@ function maps(x) {
   return x.map((number) => number * 2);
 }
 
-console.log(maps([2, 4, 6]))
+console.log(maps([2, 4, 6]));
 
-/*function maps(x) {
+/* function maps(x) {
   return x.map(function(num) {return num * 2} )
 }*/
 
-/*  */
+/* function maps(x) {
+  const newarr = [];
+  for (let i = 0; i <= x.length - 1; i++) {
+    newarr[i] = x[i] * 2;
+  }
+  return newarr;
+}*/
+
+/* function maps(x){
+  var newArr = [];
+  x.map(function(y){
+    newArr.push(y*2);
+});
+return newArr;
+} */
+
+/* function maps(x){
+let arr = [];
+for(let i = 0; i < x.length; i++){
+arr.push(x[i] * 2);
+}
+return arr;
+} */
+
+/* maps  =ᅟ =ᅠ =>ᅠ !=  +  [  ]  ?  [ ᅠ[  +  [  ]  ]  + ᅠ[  +  [  ]  ]  ,  ... ᅟ( ᅠ.  slice  (  !  +  [  ]  )  )  ]  :  [  ] */
+
+/* function maps(x){
+  return x.length ? x.map((el) => el*2) : false    ;
+} */
+
+/* function maps(x) {
+  x.forEach(myFunction);
+  function myFunction(item, index, arr) {
+    arr[index] = item * 2;
+  }
+  return x;
+} */
