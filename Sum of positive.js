e2dab7743c03e63ff5e3a6b6347fac8c80d7bbd0
ChemTi <79016313+ChemTi@users.noreply.github.com>
2022-03-04 23:26:00 +0300

    Update Sum of positive.js

--- Sum of positive.js
+++ Sum of positive.js
@@ -4,5 +4,20 @@ Example [1,-4,7,12] => 1 + 7 + 12 = 20
 Note: if there is nothing to sum, the sum is default to 0. */
 
 function positiveSum(arr) {
-    
+  const reducer = (accumulator, current) =>
+    accumulator + (current > 0 ? current : 0);
+  return arr.reduce(reducer, 0);
 }
+
+console.log(positiveSum([1, -4, 7, 12])); //20
+console.log(positiveSum([1, -2, 3, 4, 5])); //13
+console.log(positiveSum([])); //0
+console.log(positiveSum([-1, -2, -3, -4, -5])); //0
+console.log(positiveSum([-1, 2, 3, 4, -5])); //9
+
+/* function positiveSum(arr) {
+  function checkNegative(sum, current) {
+    return sum + (current > 0 ? current : 0);
+  }
+  return arr.reduce(checkNegative, 0);
+}*/
